1422dfbf2ede4d0350a1606faa86151b8c927348
emirhankarakoc <emirhankarakoc@yahoo.com>
2023-01-07 00:06:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double n = 3;
+        double toplam = 0;
+                for(double i = 1;i<=n;i++){
+                    toplam = toplam + 1/i;}
+        System.out.println(toplam);
+    }
+}
